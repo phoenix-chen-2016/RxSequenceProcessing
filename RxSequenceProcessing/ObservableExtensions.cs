d1b17d75a742c513ac6499b600860d9e65b19304
Phoenix <fphoenix923@hotmail.com>
2020-06-26 17:48:06 +0800

    加入專案檔案。

---
+++ RxSequenceProcessing/ObservableExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+
+namespace RxSequenceProcessing
+{
+	public static class ObservableExtensions
+	{
+		public static IDisposable Processing<T>(this IObservable<T> source, Func<T[], Task> callback)
+		{
+			var closer = new Subject<Unit>();
+			var prevTask = Task.CompletedTask;
+
+			return source
+				.Window(() => closer)
+				.Select(window =>
+				{
+					var tcs = new TaskCompletionSource<object>();
+					var currentTask = tcs.Task;
+
+					window.FirstAsync()
+						.Subscribe(async _ =>
+						{
+							await prevTask.ConfigureAwait(false);
+
+							prevTask = currentTask;
+							closer.OnNext(Unit.Default);
+						});
+					return (window, tcs);
+				})
+				.Subscribe(tp =>
+				{
+					tp.window
+						.ToArray()
+						.Subscribe(async n =>
+						{
+							await callback(n).ConfigureAwait(false);
+							tp.tcs.SetResult(true);
+						});
+				});
+
+
+		}
+	}
+}
---
+++ RxSequenceProcessing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace RxSequenceProcessing
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var source = Observable.Interval(TimeSpan.FromMilliseconds(100));
+
+			var rand = new Random();
+
+			source
+				.Processing(async n =>
+				{
+					Console.WriteLine("{0}", string.Join(",", n));
+
+					await Task.Delay(rand.Next(100, 5001)).ConfigureAwait(false);
+				});
+
+			Console.ReadKey();
+		}
+	}
+}
